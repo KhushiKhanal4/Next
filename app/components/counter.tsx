590ac2db63589488b72df537098805f112af8461
KhushiKhanal4 <khanalkhushi123@gmail.com>
2025-09-25 13:55:35 +0545

    Components-server/client

---
+++ app/components/counter.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { use, useState } from "react";
+
+export const Counter =()=> {
+ console.log("Counter component rendered");
+ const [count, setCount] = useState(0);
+    return (
+        <div>
+            <button onClick={() => setCount(count + 1)}>Clicked {count} times</button>
+        </div>
+    );
+}
